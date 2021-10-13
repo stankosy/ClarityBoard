1e8c56ce5adb6a8bc2e815bdf9e60aec1721b896
Stan <stan.kosyakov@gmail.com>
2021-10-13 23:33:02 +0800

    added useState to button

--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const positions = [
   {
     id: 1,
@@ -47,6 +49,13 @@ const positions = [
 ];
 
 export default function CardsList(props) {
+
+  const [title, setTitle] = useState(props.title)
+
+  const updateTitle = () => {
+    setTitle('updated')
+  }
+
   return (  
     <div className="bg-white shadow-xl rounded-md">
       <div className=" px-4 py-2 border-b border-gray-200 sm:px-6 ">
@@ -75,6 +84,9 @@ export default function CardsList(props) {
           </div>
           <div className="ml-4 mt-4 flex-shrink-0">
             <button
+              onClick={
+                updateTitle
+              }
               type="button"
               className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
@@ -92,7 +104,7 @@ export default function CardsList(props) {
                 <div className="px-4 py-4 sm:px-6">
                   <div className="flex items-center justify-between">
                     <p className="text-sm font-medium text-indigo-600 truncate">
-                      {position.title}
+                      {title}
                     </p>
                     <div className="ml-2 flex-shrink-0 flex">
                       <p className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
